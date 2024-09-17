37ab8065a87800d1042612fce0c2e2e029d814d7
Pardeep <pardeep.kumar@bluebash.co>
2024-09-17 13:57:22 +0530

    Upgrade dependencies for Spree 4.8

--- db/migrate/20140514194315_create_affirm_checkout.rb
+++ db/migrate/20140514194315_create_affirm_checkout.rb
@@ -1,9 +1,11 @@
-class CreateAffirmCheckout < ActiveRecord::Migration
+# frozen_string_literal: true
+
+class CreateAffirmCheckout < ActiveRecord::Migration[7.1]
   def change
     create_table :spree_affirm_checkouts do |t|
       t.string     :token
-      t.references :order
-      t.references :payment_method
+      t.references :order, foreign_key: { to_table: :spree_orders }
+      t.references :payment_method, foreign_key: { to_table: :spree_payment_methods }
       t.timestamps
     end
   end
